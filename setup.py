9c07c31731ddde6d4cab567558f6dbba1dc79a28
fireclawthefox <fireclawthefox@gmail.com>
2019-07-06 10:06:17 +0200

    updated setup script and ignore build folder

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup
 
-exclude = [
-    "Screenshots/**",
-    "Screenshots"]
+include = [
+    "**/icons/*.png",
+    "**/LICENSE"
+]
 
 setup(
-    name='DirectGUIDesigner',
+    name='DirectGuiDesigner',
     author = "Fireclaw the Fox",
     author_email = "info@grimfang-studio.org",
     options = {
         'build_apps': {
-            'include_patterns': ['**/*'],
-            #'exclude_patterns': exclude,
-            #'file_handlers':  {'.egg': copyEgg},
+            'include_patterns': include,
             'gui_apps': {
                 'directguidesigner': 'DirectGuiDesigner.py',
             },
