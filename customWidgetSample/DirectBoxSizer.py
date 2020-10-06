677ad01fa6557579479579ac7516397aca613944
fireclawthefox <fireclawthefox@gmail.com>
2020-10-06 22:54:53 +0200

    Fixed and enhanced DirectBoxSizer widget
    Better position calculation
    New auto framesize update feature
    mae refresh function available from designer

--- customWidgetSample/DirectBoxSizer.py
+++ customWidgetSample/DirectBoxSizer.py
@@ -13,13 +13,13 @@ DGG.HORIZONTAL_INVERTED = 'horizontal_inverted'
 class DirectItemContainer():
     def __init__(self, element, **kw):
         self.element = element
-        if "stretch" in kw:
-            if kw.get("stretch"):
-                pass
-            del kw["stretch"]
-        self.margin = VBase4(0,0,0,0)
-        if "margin" in kw:
-            self.margin = kw.get("margin")
+        #if "stretch" in kw:
+        #    if kw.get("stretch"):
+        #        pass
+        #    del kw["stretch"]
+        #self.margin = VBase4(0,0,0,0)
+        #if "margin" in kw:
+        #    self.margin = kw.get("margin")
 
 class DirectBoxSizer(DirectFrame):
     """
@@ -33,8 +33,9 @@ class DirectBoxSizer(DirectFrame):
             ('numStates',      1,          None),
             ('state',          DGG.NORMAL, None),#self.inactiveInitState, None),
 
-            ('orientation',    DGG.HORIZONTAL, None),
+            ('orientation',    DGG.HORIZONTAL, self.refresh),
             ('itemMargin',     (0,0,0,0),  None),
+            ('autoUpdateFrameSize', True,  None),
             )
         # Merge keyword options with default options
         self.defineoptions(kw, optiondefs)
@@ -63,47 +64,88 @@ class DirectBoxSizer(DirectFrame):
         return 0
 
     def refresh(self):
-        bounds = self.bounds
+        # sanity check so we don't get here to early
+        if not hasattr(self, "bounds"): return
+        nextX = 0
+        nextY = 0
+
+        sizer_pad = self["pad"]
+
+        b_top = 0
+        b_bottom = 0
+        b_left = 0
+        b_right = 0
+
+        margin_left = 0#self["itemMargin"][0]
+        margin_right = 0#self["itemMargin"][1]
+        margin_bottom = 0#self["itemMargin"][2]
+        margin_top = 0#self["itemMargin"][3]
+
+        if self["autoUpdateFrameSize"]:
+            self["frameSize"] = (0, 0, 0, 0)
 
         if self['orientation'] == DGG.HORIZONTAL:
             # Left to Right
-            nextX = bounds[0]
             for item in self["items"]:
                 curElem = item.element
                 scale = curElem.getScale()[0]
-                elemBounds = curElem.bounds
-                curElem.setPos(nextX - elemBounds[0]*scale + item.margin[0], 0, self.getCenter()[1])
-                nextX = nextX + curElem.getWidth()*scale + item.margin[0] + item.margin[1]
+                pad = curElem["pad"][0]
+                bw = -curElem["borderWidth"][0] * scale
+                bound = curElem.bounds[0] * scale
+                curElem.setPos(nextX - bound + margin_left - bw + sizer_pad[0], 0, sizer_pad[1])
+                nextX = nextX + curElem.getWidth()*scale + margin_left + margin_right - pad - bw*2
+
+                b_top = max(b_top, (curElem.bounds[3]*scale + pad - bw))
+                b_bottom = min(b_bottom, (curElem.bounds[2]*scale - pad + bw))
+                b_right = nextX
 
         elif self['orientation'] == DGG.HORIZONTAL_INVERTED:
             # Right to Left
-            nextX = bounds[1]
             for item in self["items"]:
                 curElem = item.element
                 scale = curElem.getScale()[0]
-                elemBounds = curElem.bounds
-                curElem.setPos(nextX - elemBounds[1]*scale - item.margin[1], 0, self.getCenter()[1])
-                nextX = nextX - curElem.getWidth()*scale - item.margin[0] - item.margin[1]
+                pad = -curElem["pad"][0]
+                bw = curElem["borderWidth"][0] * scale
+                bound = curElem.bounds[1] * scale
+                curElem.setPos(nextX - bound - margin_right + bw + sizer_pad[0], 0, sizer_pad[1])
+                nextX = nextX - curElem.getWidth()*scale - margin_left - margin_right + pad + bw*2
+
+                b_top = max(b_top, (curElem.bounds[3]*scale + pad - bw))
+                b_bottom = min(b_bottom, (curElem.bounds[2]*scale + pad - bw))
+                b_left = nextX
 
         elif self['orientation'] == DGG.VERTICAL:
             # Top to Bottom
-            nextY = bounds[3]
             for item in self["items"]:
                 curElem = item.element
                 scale = curElem.getScale()[2]
-                elemBounds = curElem.bounds
-                curElem.setPos(self.getCenter()[0], 0, nextY - elemBounds[3]*scale - item.margin[3])
-                nextY = nextY - curElem.getHeight()*scale - item.margin[3] - item.margin[2]
+                pad = curElem["pad"][1]
+                bw = curElem["borderWidth"][1] * scale
+                bound = curElem.bounds[3] * scale
+                curElem.setPos(sizer_pad[0], 0, nextY - bound - margin_top - bw + sizer_pad[1])
+                nextY = nextY - curElem.getHeight()*scale - margin_top - margin_bottom - pad - bw*2
+
+                b_bottom = nextY
+                b_left = min(b_left, (curElem.bounds[0]*scale - pad - bw))
+                b_right = max(b_right, (curElem.bounds[1]*scale + pad + bw))
 
         elif self['orientation'] == DGG.VERTICAL_INVERTED:
             # Bottom to Top
-            nextY = bounds[2]
             for item in self["items"]:
                 curElem = item.element
                 scale = curElem.getScale()[2]
-                elemBounds = curElem.bounds
-                curElem.setPos(self.getCenter()[0], 0, nextY - elemBounds[2]*scale + item.margin[2])
-                nextY = nextY + curElem.getHeight()*scale + item.margin[3] + item.margin[2]
+                pad = curElem["pad"][1]
+                bw = curElem["borderWidth"][1] * scale
+                bound = curElem.bounds[2] * scale
+                curElem.setPos(sizer_pad[0], 0, nextY - bound + margin_bottom + bw + sizer_pad[1])
+                nextY = nextY + curElem.getHeight()*scale + margin_top + margin_bottom + pad + bw*2
+
+                b_top = nextY
+                b_left = min(b_left, (curElem.bounds[0]*scale - pad - bw))
+                b_right = max(b_right, (curElem.bounds[1]*scale + pad + bw))
 
         else:
             raise ValueError('Invalid value for orientation: %s' % (self['orientation']))
+
+        if self["autoUpdateFrameSize"]:
+            self["frameSize"] = (b_left+sizer_pad[0], b_right+sizer_pad[0], b_bottom+sizer_pad[1], b_top+sizer_pad[1])
