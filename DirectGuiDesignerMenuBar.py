4c263f726f63fc7101d21d15d5b39d1cef680825
fireclawthefox <fireclawthefox@gmail.com>
2021-02-22 14:21:36 +0100

    Use menu items and sizers from DirectGUI extension

--- DirectGuiDesignerMenuBar.py
+++ DirectGuiDesignerMenuBar.py
@@ -8,8 +8,8 @@ DGG.BELOW = "below"
 from direct.gui.DirectButton import DirectButton
 from direct.gui.DirectFrame import DirectFrame
 from direct.gui.DirectCheckBox import DirectCheckBox
-#from direct.gui.DirectOptionMenu import DirectOptionMenu
-from directGuiOverrides.DirectOptionMenu import DirectOptionMenu
+from DirectGuiExtension.DirectMenuItem import DirectMenuItem, DirectMenuItemEntry, DirectMenuItemSubMenu
+from DirectGuiExtension.DirectBoxSizer import DirectBoxSizer
 
 class DirectGuiDesignerMenuBar(DirectObject):
     def __init__(self, tooltip, grid):
@@ -28,104 +28,106 @@ class DirectGuiDesignerMenuBar(DirectObject):
         #
         # Menubar
         #
-        self.menuBar = DirectFrame(
+        self.menuBar = DirectBoxSizer(
             frameColor=(0.25, 0.25, 0.25, 1),
             frameSize=(0,screenWidthPx,-12, 12),
+            autoUpdateFrameSize=False,
             pos=(0, 0, -12),
             parent=base.pixel2d)
 
-        x = 0
-
-        self.file = DirectOptionMenu(
+        fileEntries = [
+            DirectMenuItemEntry("New", base.messenger.send, ["newProject"]),
+            DirectMenuItemEntry("Save", base.messenger.send, ["saveProject"]),
+            DirectMenuItemEntry("Load", base.messenger.send, ["loadProject"]),
+            DirectMenuItemEntry("Export", base.messenger.send, ["exportProject"]),
+            DirectMenuItemEntry("Quit", base.messenger.send, ["quitApp"]),
+            ]
+        self.file = DirectMenuItem(
+            text="File",
             text_fg=(1,1,1,1),
             text_scale=0.8,
-            items=["New", "Save", "Load", "Export", "Quit"],
-            pos=(x, 0, -5),
+            items=fileEntries,
             frameSize=(0,65/21,-7/21,17/21),
             frameColor=color,
             scale=21,
             relief=DGG.FLAT,
             item_text_fg=(1,1,1,1),
             item_text_scale=0.8,
-            highlightScale=(0.8,0.8),
             item_relief=DGG.FLAT,
-            item_frameColor=color,
             item_pad=(0.2, 0.2),
-            highlightColor=color[2],
-            popupMenuLocation=DGG.BELOW,
-            command=self.toolbarFileCommand,
-            parent=self.menuBar)
-        self.file["text"] = "File"
+            itemFrameColor=color,
+            popupMenu_itemMargin=(0,0,-.1,-.1),
+            popupMenu_frameColor=color,
+            highlightColor=color[2])
 
-        x += 65
-
-        self.view = DirectOptionMenu(
+        # NOTE: view entries defined after toolbar creation
+        self.view = DirectMenuItem(
+            text="View",
             text_fg=(1,1,1,1),
             text_scale=0.8,
-            items=["Toggle Grid", "Toggle Scale"],
-            pos=(x, 0, -5),
+            items=[DirectMenuItemEntry("placeholder", print, ["placeholder"])],
             frameSize=(0,65/21,-7/21,17/21),
             frameColor=color,
             scale=21,
             relief=DGG.FLAT,
             item_text_fg=(1,1,1,1),
             item_text_scale=0.8,
-            highlightScale=(0.8,0.8),
             item_relief=DGG.FLAT,
-            item_frameColor=color,
             item_pad=(0.2, 0.2),
-            highlightColor=color[2],
-            popupMenuLocation=DGG.BELOW,
-            command=self.toolbarViewCommand,
-            parent=self.menuBar)
-        self.view["text"] = "View"
+            itemFrameColor=color,
+            popupMenu_itemMargin=(0,0,-.1,-.1),
+            popupMenu_frameColor=color,
+            highlightColor=color[2])
 
-        x += 65
-
-        self.tools = DirectOptionMenu(
+        toolsEntries = [
+            DirectMenuItemEntry("Delete Element", base.messenger.send, ["removeElement"]),
+            DirectMenuItemEntry("Options", base.messenger.send, ["showSettings"]),
+            DirectMenuItemEntry("Copy", base.messenger.send, ["copyElement"]),
+            DirectMenuItemEntry("Paste", base.messenger.send, ["pasteElement"]),
+            DirectMenuItemEntry("Copy options", base.messenger.send, ["copyOptions"]),
+            DirectMenuItemEntry("Paste options", base.messenger.send, ["pasteOptions"]),
+            DirectMenuItemEntry("Help", base.messenger.send, ["showHelp"]),
+        ]
+        self.tools = DirectMenuItem(
+            text="Tools",
             text_fg=(1,1,1,1),
             text_scale=0.8,
-            items=["Delete Element", "Copy", "Paste", "Copy options", "Paste options", "Options", "Help"],
-            pos=(x, 0, -5),
+            items=toolsEntries,
             frameSize=(0,65/21,-7/21,17/21),
             frameColor=color,
             scale=21,
             relief=DGG.FLAT,
             item_text_fg=(1,1,1,1),
             item_text_scale=0.8,
-            highlightScale=(0.8,0.8),
             item_relief=DGG.FLAT,
-            item_frameColor=color,
             item_pad=(0.2, 0.2),
-            highlightColor=color[2],
-            popupMenuLocation=DGG.BELOW,
-            command=self.toolbarToolsCommand,
-            parent=self.menuBar)
-        self.tools["text"] = "Tools"
+            itemFrameColor=color,
+            popupMenu_itemMargin=(0,0,-.1,-.1),
+            popupMenu_frameColor=color,
+            highlightColor=color[2])
 
+        self.menuBar.addItem(self.file)
+        self.menuBar.addItem(self.view)
+        self.menuBar.addItem(self.tools)
 
         #
         # Toolbar
         #
-        self.toolBar = DirectFrame(
+        self.toolBar = DirectBoxSizer(
             frameColor=(0.25, 0.25, 0.25, 1),
             frameSize=(0,barWidth,-24, 24),
+            autoUpdateFrameSize=False,
             pos=(left, 0, -24-24),
             parent=base.pixel2d)
 
-        x = self.toolBar.bounds[0]
         buttonColor = (
             (0.8, 0.8, 0.8, 1), # Normal
             (0.9, 0.9, 1, 1), # Click
             (0.8, 0.8, 1, 1), # Hover
             (0.5, 0.5, 0.5, 1)) # Disabled
         btn = DirectButton(
-            parent=self.toolBar,
             frameSize=(-24,24,-24,24),
             frameColor=buttonColor,
-            pos=(x + 24, 0, 0),
-            scale=1,
-            #text_scale=0.33,
             relief=DGG.FLAT,
             command=base.messenger.send,
             extraArgs=["newProject"],
@@ -134,14 +136,11 @@ class DirectGuiDesignerMenuBar(DirectObject):
         btn.setTransparency(TransparencyAttrib.M_multisample)
         btn.bind(DGG.ENTER, self.tt.show, ["Create New GUI (Ctrl-N)"])
         btn.bind(DGG.EXIT, self.tt.hide)
-        x += 48
+        self.toolBar.addItem(btn)
+
         btn = DirectButton(
-            parent=self.toolBar,
             frameSize=(-24,24,-24,24),
             frameColor=buttonColor,
-            pos=(x + 24, 0, 0),
-            scale=1,
-            #text_scale=0.33,
             relief=DGG.FLAT,
             command=base.messenger.send,
             extraArgs=["saveProject"],
@@ -150,13 +149,11 @@ class DirectGuiDesignerMenuBar(DirectObject):
         btn.setTransparency(TransparencyAttrib.M_multisample)
         btn.bind(DGG.ENTER, self.tt.show, ["Save GUI as gui Project (Ctrl-S)"])
         btn.bind(DGG.EXIT, self.tt.hide)
-        x += 48
+        self.toolBar.addItem(btn)
+
         btn = DirectButton(
-            parent=self.toolBar,
             frameSize=(-24,24,-24,24),
             frameColor=buttonColor,
-            pos=(x + 24, 0, 0),
-            scale=1,
             text_scale=0.33,
             relief=DGG.FLAT,
             command=base.messenger.send,
@@ -166,13 +163,11 @@ class DirectGuiDesignerMenuBar(DirectObject):
         btn.setTransparency(TransparencyAttrib.M_multisample)
         btn.bind(DGG.ENTER, self.tt.show, ["Export GUI as python script (Ctrl-E)"])
         btn.bind(DGG.EXIT, self.tt.hide)
-        x += 48
+        self.toolBar.addItem(btn)
+
         btn = DirectButton(
-            parent=self.toolBar,
             frameSize=(-24,24,-24,24),
             frameColor=buttonColor,
-            pos=(x + 24, 0, 0),
-            scale=1,
             relief=DGG.FLAT,
             text_scale=0.33,
             command=base.messenger.send,
@@ -182,12 +177,11 @@ class DirectGuiDesignerMenuBar(DirectObject):
         btn.setTransparency(TransparencyAttrib.M_multisample)
         btn.bind(DGG.ENTER, self.tt.show, ["Load GUI project (Ctrl-O)"])
         btn.bind(DGG.EXIT, self.tt.hide)
-        x += 48 + 12
+        self.toolBar.addItem(btn)
+
         btn = DirectButton(
-            parent=self.toolBar,
+            frameSize=(-24,24,-24,24),
             frameColor=buttonColor,
-            pos=(x + 24, 0, 0),
-            scale=1,
             relief=DGG.FLAT,
             text_scale=0.33,
             command=base.messenger.send,
@@ -197,13 +191,11 @@ class DirectGuiDesignerMenuBar(DirectObject):
         btn.setTransparency(TransparencyAttrib.M_multisample)
         btn.bind(DGG.ENTER, self.tt.show, ["Delete selected element (Del)"])
         btn.bind(DGG.EXIT, self.tt.hide)
-        x += 48
+        self.toolBar.addItem(btn)
+
         self.cb_grid = DirectCheckBox(
-            parent=self.toolBar,
             frameSize=(-24,24,-24,24),
             frameColor=buttonColor,
-            pos=(x + 24, 0, 0),
-            scale=1,
             relief=DGG.FLAT,
             text_scale=12,
             image="icons/GridOff.png" if self.grid.isHidden() else "icons/GridOn.png",
@@ -215,13 +207,11 @@ class DirectGuiDesignerMenuBar(DirectObject):
         self.cb_grid.setTransparency(TransparencyAttrib.M_multisample)
         self.cb_grid.bind(DGG.ENTER, self.tt.show, ["Toggle Grid (Ctrl-G)"])
         self.cb_grid.bind(DGG.EXIT, self.tt.hide)
-        x += 48
+        self.toolBar.addItem(self.cb_grid)
+
         self.cb_scale = DirectCheckBox(
-            parent=self.toolBar,
             frameSize=(-24,24,-24,24),
             frameColor=buttonColor,
-            pos=(x + 24, 0, 0),
-            scale=1,
             relief=DGG.FLAT,
             text_scale=12,
             image="icons/Scale1.png",
@@ -233,13 +223,11 @@ class DirectGuiDesignerMenuBar(DirectObject):
         self.cb_scale.setTransparency(TransparencyAttrib.M_alpha)
         self.cb_scale.bind(DGG.ENTER, self.tt.show, ["Toggle editor scale (Aspect/Pixel)"])
         self.cb_scale.bind(DGG.EXIT, self.tt.hide)
-        x += 48 + 12
+        self.toolBar.addItem(self.cb_scale)
+
         btn = DirectButton(
-            parent=self.toolBar,
             frameSize=(-24,24,-24,24),
             frameColor=buttonColor,
-            pos=(x + 24, 0, 0),
-            scale=1,
             relief=DGG.FLAT,
             text_scale=0.33,
             command=base.messenger.send,
@@ -249,13 +237,11 @@ class DirectGuiDesignerMenuBar(DirectObject):
         btn.setTransparency(TransparencyAttrib.M_alpha)
         btn.bind(DGG.ENTER, self.tt.show, ["Quit Direct GUI Designer (Ctrl-Q)"])
         btn.bind(DGG.EXIT, self.tt.hide)
-        x += 48 + 12
+        self.toolBar.addItem(btn)
+
         btn = DirectButton(
-            parent=self.toolBar,
             frameSize=(-24,24,-24,24),
             frameColor=buttonColor,
-            pos=(x + 24, 0, 0),
-            scale=1,
             relief=DGG.FLAT,
             text_scale=0.33,
             command=base.messenger.send,
@@ -265,13 +251,11 @@ class DirectGuiDesignerMenuBar(DirectObject):
         btn.setTransparency(TransparencyAttrib.M_multisample)
         btn.bind(DGG.ENTER, self.tt.show, ["Show a help Dialog (F1)"])
         btn.bind(DGG.EXIT, self.tt.hide)
-        x += 48 + 12
+        self.toolBar.addItem(btn)
+
         btn = DirectButton(
-            parent=self.toolBar,
             frameSize=(-24,24,-24,24),
             frameColor=buttonColor,
-            pos=(x + 24, 0, 0),
-            scale=1,
             relief=DGG.FLAT,
             text_scale=0.33,
             command=base.messenger.send,
@@ -281,51 +265,28 @@ class DirectGuiDesignerMenuBar(DirectObject):
         btn.setTransparency(TransparencyAttrib.M_multisample)
         btn.bind(DGG.ENTER, self.tt.show, ["Show Designer Settings"])
         btn.bind(DGG.EXIT, self.tt.hide)
+        self.toolBar.addItem(btn)
 
         if not ConfigVariableBool("show-toolbar", True).getValue():
             self.toolBar.hide()
 
+        viewEntries = [
+            DirectMenuItemEntry("Toggle Grid", self.cb_grid.commandFunc, [None]),
+            DirectMenuItemEntry("Toggle Scale", self.cb_scale.commandFunc, [None])
+        ]
+
+        self.view["items"] = viewEntries
+        # HACK: this shouldn't be needed
+        '''
+        self.view["item_text_fg"]=(1,1,1,1)
+        self.view["item_text_scale"]=0.8
+        self.view["item_relief"]=DGG.FLAT
+        self.view["item_pad"]=(0.2, 0.2)
+        self.view["itemFrameColor"]=color
+        self.view["popupMenu_itemMargin"]=(0,0,-.1,-.1)
+        self.view["popupMenu_frameColor"]=color
+        '''
         self.accept("setVisualEditorParent", self.setVisualEditorParent)
-
-    def toolbarFileCommand(self, selection):
-        if selection == "New":
-            base.messenger.send("newProject")
-        elif selection == "Save":
-            base.messenger.send("saveProject")
-        elif selection == "Load":
-            base.messenger.send("loadProject")
-        elif selection == "Export":
-            base.messenger.send("exportProject")
-        elif selection == "Quit":
-            base.messenger.send("quitApp")
-
-        self.file["text"] = "File"
-
-    def toolbarViewCommand(self, selection):
-        if selection == "Toggle Grid":
-            self.cb_grid.commandFunc(None)
-        elif selection == "Toggle Scale":
-            self.cb_scale.commandFunc(None)
-
-        self.view["text"] = "View"
-
-    def toolbarToolsCommand(self, selection):
-        if selection == "Delete Element":
-            base.messenger.send("removeElement")
-        elif selection == "Options":
-            base.messenger.send("showSettings")
-        elif selection == "Copy":
-            base.messenger.send("copyElement")
-        elif selection == "Paste":
-            base.messenger.send("pasteElement")
-        elif selection == "Copy options":
-            base.messenger.send("copyOptions")
-        elif selection == "Paste options":
-            base.messenger.send("pasteOptions")
-        elif selection == "Help":
-            base.messenger.send("showHelp")
-
-        self.tools["text"] = "Tools"
 
     def toggleGrid(self, selection):
         base.messenger.send("toggleGrid", [selection])
